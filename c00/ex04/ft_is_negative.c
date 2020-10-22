3c0b4caf609b26201fc058cfffb2f0140693cc81
Giovanni Cusumano <gcusuman@e4r11p4.42roma.it>
2020-10-22 17:04:29 +0200

    ftneg

--- c00/ex04/ft_is_negative.c
+++ c00/ex04/ft_is_negative.c
@@ -1,23 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcusuman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/22 16:59:22 by gcusuman          #+#    #+#             */
+/*   Updated: 2020/10/22 17:03:28 by gcusuman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void    ft_is_negative(int n);
-void    ft_putchar(char c);
-
-int     main(void)
-{
-    ft_is_negative(-6);
-
-    return (0);
-}
-
-void ft_is_negative(int n)
-{
-    char    result;
-    result = (n < 0) ? 'N' : 'P';
-    ft_putchar(result);
-
-}
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
+}
+
+void	ft_is_negative(int n)
+{
+	char result;
+
+	if (n < 0)
+	{
+		result = 'N';
+	}
+	else
+	{
+		result = 'P';
+	}
+	ft_putchar(result);
 }
