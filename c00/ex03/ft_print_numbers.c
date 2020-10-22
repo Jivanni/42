9e27a6f9a5fb4aafc9cc9cce996e8c0b05fb627e
Giovanni Cusumano <gcusuman@e4r11p4.42roma.it>
2020-10-22 16:49:58 +0200

    main

--- c00/ex03/ft_print_numbers.c
+++ c00/ex03/ft_print_numbers.c
@@ -3,37 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_numbers.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcusuman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gcusuman <gcusuman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 16:32:34 by gcusuman          #+#    #+#             */
-/*   Updated: 2020/10/22 16:32:42 by gcusuman         ###   ########.fr       */
+/*   Updated: 2020/10/22 16:49:38 by gcusuman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void    ft_print_reverse_alphabet(void);
-void    ft_putchar(char c);
-
-int     main(void)
+void	ft_print_numbers(void)
 {
-    ft_print_reverse_alphabet();
-
-    return (0);
-}
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void    ft_print_reverse_alphabet(void)
-
-{
-    char i = '0';
-    while (i <= '9')
-    {
-        ft_putchar(i);
-        i++;
-    }
+	write(1, "0123456789", 26);
 }
