9d6539546665342e9956d3917fdfc18c0d693d3a
Giovanni Cusumano <gcusuman@e3r1p2.42roma.it>
2020-10-24 20:22:55 +0200

    aa

---
+++ c01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+void ft_rev_int_tab(int *tab, int size);
+
+int main()
+{
+	char array[4] = {1, 2, 3, 4};
+	char *ptr = &array[0];
+	
+	ft_rev_int_tab(ptr, 4)
+
+	printf("%d\n",array[0]);
+}
+
+
+void ft_rev_int_tab(int *tab, int size)
+{
+	int temp[size]
+}
