e3295a6f611274eccfa1963de41fb8b0bc1cd342
Giovanni Cusumano <gcusuman@e3r8p5.42roma.it>
2020-10-25 18:00:00 +0100

    ss

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -5,15 +5,15 @@ void ft_rev_int_tab(int *tab, int size);
 
 int main()
 {
-	int array[6] = {1, 2, 3, 4, 5, 6};
-	int *ptr = &array[0];
+	int array[7] = {1, 2, 3, 4, 5, 6, 7};
+	int *ptr = array;
 	
-	ft_rev_int_tab(ptr, 6);
+	ft_rev_int_tab(ptr, 7);
 
-	printf("%d\n", *(ptr + 5));
 
-	printf("%d,%d,%d,%d,%d,%d\n",array[0],array[1],array[2],array[3],array[4],array[5]);
+	printf("%d,%d,%d,%d,%d,%d,%d\n",array[0],array[1],array[2],array[3],array[4],array[5], array[6]);
 }
+
 
 void ft_rev_int_tab(int *tab, int size)
 {
@@ -21,9 +21,9 @@ void ft_rev_int_tab(int *tab, int size)
 	int y;
 	
 	x = 0;
-	y = size;
-	
-	while (x != y)
+	y = size - 1;
+
+	while (x < y)
 	{	
 		int temp;
 		temp = *(tab + x);
