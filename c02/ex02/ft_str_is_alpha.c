f39fe3c943caac09974716f1450d8bd63cca24e3
Giovanni Cusumano <gcusuman@e4r9p2.42roma.it>
2020-10-26 17:44:07 +0100

    aa

--- c02/ex02/ft_str_is_alpha.c
+++ c02/ex02/ft_str_is_alpha.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 
 int ft_str_is_alpha(char *str);
@@ -5,12 +6,23 @@ int ft_str_is_alpha(char *str);
 int main()
 {
 
-char string[7] = "abcdef";
+char string[6] = "";
+char *ptr = string;
 
 
-printf("%s\n",ft_strncpy(dest, string, 10));
+printf("%d\n",ft_str_is_alpha(ptr));
 
 } 
+*/
 
-
-int ft_str_is_alpha(char *str);
+int ft_str_is_alpha(char *str)
+{
+    while (*++str)
+    {
+        if (!((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z')))
+        {
+            return (0); 
+        }
+    }
+    return(1);
+}
