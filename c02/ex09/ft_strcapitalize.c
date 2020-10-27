dd5e8b96f9a90a7ef052b800337593b5b070375b
Giovanni Cusumano <gcusuman@e3r6p8.42roma.it>
2020-10-27 10:24:06 +0100

    ff

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 char *ft_strcapitalize(char *str);
+char *ft_strupcase(char *str);
+char *ft_strlowcase(char *str);
+int char_isalphan (char *str);
 
 int main()
 {
@@ -19,6 +22,24 @@ char *ft_strcapitalize(char *str)
     i = 0;
     while (str[i])
     {   
+        if (i == 0 || char_isalphan(&str[i-1]) )
+        {
+            str[i] = *ft_strupcase(&str[i]);
+        }
+        else str[i] = *ft_strlowcase(&str[i]);
+
+        i++;
+    }   
+    return str;
+}
+
+
+char *ft_strupcase(char *str)
+{
+    int i;
+    i = 0;
+    while (str[i])
+    {   
         if (str[i] >= 'a' && str[i] <= 'z')
         {
         str[i] -= 32; 
@@ -26,4 +47,24 @@ char *ft_strcapitalize(char *str)
         i++;
     }   
     return str;
+}
+
+char    *ft_strlowcase(char *str)
+{
+    int i;
+    i = 0;
+    while (str[i])
+    {   
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+        str[i] += 32; 
+        }
+        i++;
+    }   
+    return str;
+}
+
+int char_isalphan (char *str)
+{
+    return (!((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z') || (*str >= '0' && *str <= '9')));
 }
