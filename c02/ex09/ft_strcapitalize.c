8bd4c23e330890c9d043e1850be0d4d66c1b17e5
Giovanni Cusumano <gcusuman@e3r6p7.42roma.it>
2020-10-28 15:27:38 +0100

    aa

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,9 +6,29 @@
 /*   By: gcusuman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/27 19:09:44 by gcusuman          #+#    #+#             */
-/*   Updated: 2020/10/27 19:23:41 by gcusuman         ###   ########.fr       */
+/*   Updated: 2020/10/28 15:23:38 by gcusuman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+char	*ft_charupcase(char *str)
+{
+	if (*str >= 'a' && *str <= 'z')
+		*str -= 32;
+	return (str);
+}
+
+char	*ft_charlowcase(char *str)
+{
+	if (*str >= 'A' && *str <= 'Z')
+		*str += 32;
+	return (str);
+}
+
+int		char_isalphan(char *str)
+{
+	return (!((*str >= 'A' && *str <= 'Z') \
+	|| (*str >= 'a' && *str <= 'z') || (*str >= '0' && *str <= '9')));
+}
 
 char	*ft_strcapitalize(char *str)
 {
@@ -18,36 +38,10 @@ char	*ft_strcapitalize(char *str)
 	while (str[i])
 	{
 		if (i == 0 || char_isalphan(&str[i - 1]))
-		{
 			str[i] = *ft_charupcase(&str[i]);
-		}
 		else
 			str[i] = *ft_charlowcase(&str[i]);
 		i++;
 	}
 	return (str);
-}
-
-char	*ft_charupcase(char *str)
-{
-	if (*str >= 'a' && *str <= 'z')
-	{
-		*str -= 32;
-	}
-	return (str);
-}
-
-char	*ft_charlowcase(char *str)
-{
-	if (*str >= 'A' && *str <= 'Z')
-	{
-		*str += 32;
-	}
-	return (str);
-}
-
-int		char_isalphan(char *str)
-{
-	return (!((*str >= 'A' && *str <= 'Z') \
-	|| (*str >= 'a' && *str <= 'z') || (*str >= '0' && *str <= '9')));
 }
