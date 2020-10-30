051f35c1b27d6a6e56ed7292e3e7f491ccab665d
Giovanni Cusumano <gcusuman@e3r7p5.42roma.it>
2020-10-30 11:39:19 +0100

    asdsad

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -1,15 +1,68 @@
-int		ft_atoi(char *str)
+int		ft_isspace(char *str)
 {
-	return (str);
+	return(*str == '\t' || *str == '\n' || *str == '\v' || *str == '\f' || *str == '\r' || *str == ' ');
 }
 
+int		ft_char_isnum(char *str)
+{
+	return (*str >= '0' && *str <= '9');
+}
+
+int		strtoi(char *c, unsigned int l)
+{
+    if (l)
+	{
+		return (c[l-1] - '0' + 10 * strtoi(c, l - 1));
+	}
+	else
+		return (0);
+}
+
+int		ft_atoi(char *str)
+{
+	int sign;
+	unsigned int num;
+	int numlen;
+
+	num = 0;
+	sign = 1;
+	numlen = 0;
+	while(*str && ft_isspace(str))
+	{
+		str++;
+	}
+	while(*str == '-' || *str == '+' )
+	{
+		switch (*str)
+		{
+		case '-':
+			sign *= -1;
+			break;
+		case '+':
+			sign *= +1;
+			break;
+		default:
+			break;
+		}
+		str++;
+	}
+	while(ft_char_isnum(str))
+	{
+		numlen++;
+		str++;
+	}
+	str -= numlen;
+	num = strtoi(str, numlen);
+
+	return (sign * num);
+}
 #include <stdio.h>
 
 int main()
 {
-	char test[] = " ---+--+1234ab567";
-	printf("%s", ft_atoi(test));
+	char test[] = "  --134ab567";
+
+	printf("%d", ft_atoi(test));
 }
 
 
-//``\t''   ``\n''    ``\v''    ``\f''    ``\r''    `` ''
