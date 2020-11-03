63e4c789f146adcf9c5041ed447803989035ccc4
Giovanni Cusumano <gcusuman@e3r2p6.42roma.it>
2020-11-03 18:42:46 +0100

    ss

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -1,49 +1,68 @@
 #include <unistd.h>
 
 //check matrix
-int sort_str(int curr, int argc, char **argv)
+int check_str(char *str)
 {
-	char first_ch;
-	first_ch = argv[curr][0];
+	return (str[0] == '\0');
+}
+
+int check_if_first(int curr, int argc, char **argv)
+{
 	int i;
+	int index;
+
 	i = 1;
-	char temp;
-	temp = 32;
+	index = 0;
 	while (i < argc)
 	{
-		if (i != curr)
+		while (i != curr && !check_str(argv[curr]))
 		{
-			argv[curr][i]
+			if (argv[curr][index] == argv[i][index])
+				index ++;
+			else
+				return (argv[curr][index] > argv[i][index]);
 		}
+		index = 0;
 		i++;
+		i = i % argc;
 	}
-
+	return (0);
 }
 //mark str[0] with '\0'
 void mark_str(char *str)
 {
-
+	str[0] = '\0';
 }
 // check if string has been printed
-void check_str(char *str)
-{
 
+void ft_pustr(char *ptr)
+{
+	char *start;
+	start = ptr;
+	while(*ptr)
+		write(1,ptr++,1);
+	write(1,"\n",1);
+	mark_str(start);
 }
 
 int main(int argc, char *argv[])
 {
 	char *ptr;
 	int i;
+	int printed;
 
+	printed = 1;
 	i = 1;
 	if (argc > 1)
 		{
-			while (i < argc)
+			while (printed < argc)
 			{
-				ptr = argv[i++];
-				while(*ptr)
-					write(1,ptr++,1);
-				write(1,"\n",1);
+				ptr = argv[i];
+				if(!check_str(ptr) && check_if_first(i,argc,argv))
+					ft_pustr(ptr);
+					printed++;
+				i++;
+				i = i % argc;
 			}
 		}
 }
