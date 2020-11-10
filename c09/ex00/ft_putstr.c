c650da06abcee695d206c902942654acb196ef11
Giovanni Cusumano <gcusuman@e4r13p3.42roma.it>
2020-11-10 19:55:28 +0100

    a

--- c09/ex00/ft_putstr.c
+++ c09/ex00/ft_putstr.c
@@ -12,16 +12,11 @@
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
 void	ft_putstr(char *str)
 {
 	while (*str)
 	{
-		ft_putchar(*str);
+		write(1, str, 1);
 		str++;
 	}
 }
---
+++ c10/ex00/ft_display_file.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int put_file(char *path)
+{
+	int data_stream;
+	int bytes;
+	char data[1];
+
+	data_stream = open(path, O_RDONLY);
+	while((bytes = read(data_stream,data,1)))
+		{
+			if (bytes == -1)
+				return(0);
+			write(1,&data[0],1);
+		}
+	close(data_stream);
+	return (1);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		ft_putstr("File name missing.");
+	else if (argc > 2)
+		ft_putstr("Too many arguments.");
+	else if (!(put_file(argv[1])))
+		ft_putstr("Cannot read file.");
+}
